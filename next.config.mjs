330b9a05dbedd74a43bd94d8e4827607eb7a04d4
aliriodi <aliriodi@gmail.com>
2025-03-16 12:36:58 -0300

    cargando escuelas deportivas 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["via.placeholder.com","www.google.com","plus.unsplash.com"],
+  },
 };
 
 export default nextConfig;
